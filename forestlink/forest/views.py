d0e57e4d0f3d833ba1c8b8f7e4d4114179ddcdf7
wilczu <kacpersoja1@gmail.com>
2020-12-10 22:30:13 +0000

    Add simple login page.

--- forestlink/forest/views.py
+++ forestlink/forest/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "forest/index.html")
+
+
+def login(request):
+    return render(request, "forest/login.html")
