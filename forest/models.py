dab10e876bbec75e8110ba9c93e2a0b76249d120
wilczu <kacpersoja1@gmail.com>
2020-12-11 23:37:02 +0000

    Add Blacklist table

--- forest/models.py
+++ forest/models.py
@@ -3,3 +3,10 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Blacklist(models.Model):
+    page_url = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return f"{self.page_url}"
