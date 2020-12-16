2490724391872d2029c5ecbb84536c9ccce756ea
wilczu <kacpersoja1@gmail.com>
2020-12-16 15:31:26 +0000

    Apply new relation changes between color and page tables to the views.py

--- forest/views.py
+++ forest/views.py
@@ -4,7 +4,7 @@ from django.db import IntegrityError
 from django.contrib.auth import authenticate, login, logout
 from django.core.exceptions import ObjectDoesNotExist
 
-from .models import User, Blocked, Page
+from .models import User, Blocked, Page, Color
 
 def remove_sessions(request):
     all_sessions = ['error', 'success']
@@ -67,7 +67,6 @@ def register_view(request):
 
 
 def add_page(request):
-    #TODO check the length of page name and url
     if request.user.is_authenticated:
         if request.method == 'POST':
             #Clear all sessions
@@ -75,6 +74,8 @@ def add_page(request):
             
             page_name = request.POST['title']
             page_url = request.POST['pageURL']
+            page_background = request.POST['bgColor']
+            text_color = request.POST['textColor']
 
             #Check if page name and url have correct length
             if len(page_name) <= 0 or len(page_name) > 120:
@@ -83,20 +84,30 @@ def add_page(request):
             if len(page_url) <= 0 or len(page_url) > 2000:
                 request.session['error'] = "Your page url is too shirt or too long"
                 return redirect(reverse("user"))
-                
+
             #Check if page is in banned list
             banned_list = Blocked.objects.all()
             for banned_page in banned_list:
                 if str(banned_page) in page_url:
                     request.session['error'] = f"Unfortunately <b>{page_url}</b> is blocked!"
                     return redirect(reverse("user"))  
+
+            #Saving colors to the database
+            save_colors = Color.objects.create(
+                background_color = page_background,
+                text_color = text_color
+            )
+            save_colors.save()
+
             #Saving data to the database
             save_page = Page.objects.create(
                 page_owner = request.user,
                 page_name = page_name,
-                page_url = page_url
+                page_url = page_url,
+                page_color = save_colors
             )
             save_page.save()
+
             #Set success session
             request.session['success'] = f"{page_name} was added to your desktop!"
 
