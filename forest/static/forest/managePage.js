6af056912fe8bc92649933384df7a4db69b91f71
wilczu <kacpersoja1@gmail.com>
2020-12-18 23:45:32 +0000

    Add toggleEdit function to toggle edit mode on or off.

--- forest/static/forest/managePage.js
+++ forest/static/forest/managePage.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
-    //TODO
+    $('#edit').click(() => {
+        toggleEdit();
+    });
 });
+
+function toggleEdit() {
+    //Changing content of edit menu element
+    if ($('#edit b').text() == "Edit pages") {
+        $('#edit b').text('Stop editing');
+        switchAnimation(false); //Turning on hover animation
+    } else {
+        $('#edit b').text('Edit pages');
+        switchAnimation(true);  //Turning off hover animation
+    }
+}
 
 function switchAnimation(animation) {
     let action = animation;
