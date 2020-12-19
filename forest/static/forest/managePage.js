42ef19484b679e537d5db0a82eedadfa7c9083ed
wilczu <kacpersoja1@gmail.com>
2020-12-19 16:57:59 +0000

    Add removeModal function which is showing the remove modal and passing ID to the remove button.

--- forest/static/forest/managePage.js
+++ forest/static/forest/managePage.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
     //Hide container with buttons
     switchControlButtons(true);
+    //Turning on or off Editing mode when button is pressed
     $('#edit').click(() => {
         toggleEdit();
     });
+    //Triggering removeModal function when pressing remove button
+    $('.removeButton').on('click', function() {
+        removeModal($(this).data('id'));
+    })
+
 });
 
 function toggleEdit() {
@@ -40,4 +46,11 @@ function switchControlButtons(action) {
             container.style.display = "block";
         }
     });
+}
+
+function removeModal(pageID) {
+    //Show confirmation modal
+    $('#removePage').modal('show');
+    //Passing ID value to the modal remove button
+    $('#confirmRemove').val(pageID);
 }
