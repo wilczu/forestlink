113e35bb63594896228c3b2658acec99d8c3f707
wilczu <kacpersoja1@gmail.com>
2020-12-22 23:27:15 +0000

    Add scrolling animation in js.

---
+++ forest/static/forest/js/scrolling.js
@@ -0,0 +1,20 @@
+$('document').ready(() => {
+
+    let navbar = [
+        ['page1', 'about'],
+        ['page2', 'features'],
+        ['page3', 'start'],
+        ['footerTop', 'top']
+    ];
+
+    $('a').click(() => {
+        navbar.forEach(element => {
+            $('.' + element[0]).click(() => {
+                $('body, html').animate({
+                    scrollTop: ($('#' + element[1]).offset().top)
+                },0);
+            });
+        });
+    });
+
+});
