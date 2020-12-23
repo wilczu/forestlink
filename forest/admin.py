9a2908e4556e614ad7ef599f7c045833efec4eef
wilczu <kacpersoja1@gmail.com>
2020-12-23 22:23:38 +0000

    Add report table to the admin panel.

--- forest/admin.py
+++ forest/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import User, Blocked, Page, Color
+from .models import User, Blocked, Page, Color, Report
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Blocked)
 admin.site.register(Page)
 admin.site.register(Color)
+admin.site.register(Report)
